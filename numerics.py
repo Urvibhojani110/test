508b96fcc5ab44507eedd55525e8b777d964e367
Urvibhojani110 <urvibhojani50@gmail.com>
2026-05-08 08:53:28 +0530

    My test case files added

---
+++ numerics.py
@@ -0,0 +1,29 @@
+#Numeric type (int,complex,float)
+#numeric used for calculations
+
+
+#int -> whole number (no decimal) 
+a = 10 # a is varible ,= is opreater,10 is value.
+b = 20
+print(b)
+
+print(a)
+print("a",a)
+
+print(type(a))# type to write show in output type
+print(type(b))
+
+
+#complex literal -> used in the advance math
+number = 4+5
+print(number)
+
+number = 4+5j
+print(number)
+
+
+#float-> number with decimal points
+y=3.5# numbers with decimal points value store.
+print(y)
+
+
